4450465232a3fc2df9cee703b0c61c737187b900
Swift <hellosai109@gmail.com>
2025-04-03 17:05:55 +0530

    Removed Search

--- prof_finder/urls.py
+++ prof_finder/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('login.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('search.urls')),  # Your API
 ]
--- search/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ProfessorSearchView
-
-urlpatterns = [
-    path('search/', ProfessorSearchView.as_view(), name='search'),
-]
